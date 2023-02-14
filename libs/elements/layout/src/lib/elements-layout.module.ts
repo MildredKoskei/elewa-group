600a5f54a23979818a4437c261e76af416d300de
Vincent <makokhavomondi@gmail.com>
2023-02-14 22:16:27 +0300

    :rewind: Revert ":sparkles: Enable use of our component"
    This reverts commit 219792a3095be66e7a5c68af43278aa07bb527de.

--- libs/elements/layout/src/lib/elements-layout.module.ts
+++ libs/elements/layout/src/lib/elements-layout.module.ts
@@ -5,11 +5,10 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ElewaHeroComponent } from './components/elewa-hero/elewa-hero.component';
 import { ElewaGroupMainPageComponent } from './pages/elewa-group-main-page/elewa-group-main-page.component';
-import { ElewaGroupImageAndTextBannerComponent } from '../../../../../libs/features/components/banners/src/lib/banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [HeaderComponent, ElewaGroupMainPageComponent, ElewaHeroComponent, ElewaGroupImageAndTextBannerComponent],
-  exports: [HeaderComponent, ElewaGroupMainPageComponent,ElewaHeroComponent, ElewaGroupImageAndTextBannerComponent],
+  declarations: [HeaderComponent, ElewaGroupMainPageComponent, ElewaHeroComponent],
+  exports: [HeaderComponent, ElewaGroupMainPageComponent,ElewaHeroComponent],
 })
 export class LayoutModule {}
