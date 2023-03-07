8703542463fcd4c8f0e1aa393a9ef73d09f20355
Vincent <makokhavomondi@gmail.com>
2023-03-07 16:18:24 +0300

    Make the component reusable

--- libs/pages/elewa/venture-labs/src/lib/components/venture-labs-info/venture-labs-info.component.ts
+++ libs/pages/elewa/venture-labs/src/lib/components/venture-labs-info/venture-labs-info.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-venture-labs-info',
   templateUrl: './venture-labs-info.component.html',
   styleUrls: ['./venture-labs-info.component.scss'],
 })
-export class VentureLabsInfoComponent {}
+export class VentureLabsInfoComponent {
+  Input() paragraphs = [
+    `Elewa group is funded by sustsinable, EBITDA positive, business
+    venture which lie at the core of the group. These include Elewa
+    Education, EdTech and Education consulting, and iTalanta, our
+    largest activity of IT offshoring activities.`,
+
+    `Sheltered by the group's stable business and strong cashflows,
+    Elewa's culture and Venture Labs allow for creative exploration of 
+    new business ideas and moonshot startup ventures.`
+  ]
+}
